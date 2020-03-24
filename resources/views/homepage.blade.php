2cc368c2ea1e07acd0db3ce57f229d3a737872ef
jovialcore <jollydosky@gmail.com>
2020-03-24 05:11:17 -0700

    updated with images

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,1 +1,22 @@
-dcvsdfgsdfg
+@extends('layout')
+@section('content')
+<div class="row">
+       <div class="col-9">
+ <img src="{{asset('images/show.png')}}" class="img-fluid" alt="show imges"> 
+    </div>
+   {{--  for image  --}}
+
+<div class="col-3">
+
+	<div class="txt"> {{-- our text will be here --}}
+
+			<p> Never found a great place to create professional displays 
+
+				<button class="btn btn-primary " >Create One ASAP</button>
+			</p>
+
+	 </div>
+
+</div>
+</div>
+@endsection
